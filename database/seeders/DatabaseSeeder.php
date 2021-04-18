d05d7df84566f2d32f24aebc4af8cc71f91b29d3
sonbin123 <sonbin199915@gmail.com>
2021-04-18 16:18:12 +0700

    new deploy 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        User::create([
+            'id' => 0,
+            'name' => 'admin',
+            'email' => 'admindb@gmail.com',
+            'is_admin' => 1,
+            'status' => 1,
+            'password' => \Hash::make("admin123"),
+        ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
